1c25e5e194a7218e1b58bf0c443cce4c61e53f16
Vladislav <fisunvladislav2000@gmail.com>
2024-04-23 02:43:27 +0300

    eslint rules configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,19 +35,13 @@ module.exports = {
         'no-unused-vars': 'warn',
         'react/require-default-props': 'off',
         'react/react-in-jsx-scope': 'off',
-        'react/jsx-props-no-spreading': 'warn',
         'react/function-component-definition': 'off',
+        'react/jsx-props-no-spreading':'off',
         'no-shadow': 'off',
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': [
-            'error',
-            {
-                markupOnly: true,
-                ignoreAttribute: ['data-testid', 'to'],
-            },
-        ],
+        'i18next/no-literal-string':"off",
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
