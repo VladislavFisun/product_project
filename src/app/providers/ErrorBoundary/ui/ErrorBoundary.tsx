c0920d0481dc69938cfd2142f0f7b07bcf5d8d16
Vladislav <fisunvladislav2000@gmail.com>
2024-04-23 02:59:38 +0300

    pipeline screenshot test

--- src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
+++ src/app/providers/ErrorBoundary/ui/ErrorBoundary.tsx
@@ -18,7 +18,7 @@ class ErrorBoundary
 
     static getDerivedStateFromError(error: Error) {
         // Update state so the next render will show the fallback UI.
-        return { hasError: true };
+        return { hasError:Boolean( error.message) };
     }
 
     componentDidCatch(error: Error, errorInfo: ErrorInfo) {
