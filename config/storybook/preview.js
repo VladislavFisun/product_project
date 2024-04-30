6fbc15fee0883a98055237876341d084eeaa78ed
Vladislav <fisunvladislav2000@gmail.com>
2024-04-30 15:35:43 +0300

    Storybokk && loki fix

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -3,7 +3,7 @@ import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StoreDecorator } from '../../src/shared/config/storybook/StoreDecorator/StoreDecorator';
+import { TranslationDecorator } from '../../src/shared/config/storybook/TranslationDecorator';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -18,4 +18,4 @@ export const parameters = {
 addDecorator(StyleDecorator);
 addDecorator(ThemeDecorator(Theme.LIGHT));
 addDecorator(RouterDecorator);
-addDecorator(StoreDecorator({}));
+addDecorator(TranslationDecorator);
