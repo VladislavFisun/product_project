46542912fb2b4faba393b959de7fea4bd2498df9
Vladislav <fisunvladislav2000@gmail.com>
2024-04-30 03:49:46 +0300

    add login form and user store

--- src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
+++ src/shared/config/storybook/StoreDecorator/StoreDecorator.tsx
@@ -1,8 +1,11 @@
 import { Story } from '@storybook/react';
 import StoreProvider from 'app/providers/StoreProvider/ui/StoreProvider';
 import { StateSchema } from 'app/providers/StoreProvider/config/StateSchema';
+import { Suspense } from 'react';
 
 export const StoreDecorator = (state:StateSchema) => (StoryComponent: Story) => (
 // @ts-ignore
-    <StoreProvider initialState={state}><StoryComponent /></StoreProvider>
+    <Suspense fallback={null}>
+        <StoreProvider initialState={state}><StoryComponent /></StoreProvider>
+    </Suspense>
 );
